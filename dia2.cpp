21961d7b72972580f098c5b4b41b5dae3bdb67cd
ismael <ismael.barrranco@itgranada.com>
2022-01-21 12:49:12 +0100

    booleanoooos

--- dia2.cpp
+++ dia2.cpp
@@ -9,6 +9,22 @@ int valorAbsoluto (int numero){if (numero<0){
   }
   return numero;
 }
+bool meSirve(int numMax, int numMin, int num){
+  return (numMax>num && numMin<num);
+}
+bool meSirveV2(int numMax,int numMin,int num){
+  return (num>numMax || num<numMin);
+}
+void imprimeBool(bool b){
+  if (b==0){
+    cout<<"False";
+  }
+  else{
+    cout<< "True";
+  }
+}
+
+
 int main(){
   string saludo= "hola!";
   string repetir;
@@ -19,9 +35,10 @@ int main(){
   bool diferencia;
   int numero;
   string palabra1, palabra2, palabra;
-
+  double suma=0;
+  bool ok,ok2;
+  int num1,numMin,numMax,num2;
   //Jugamos con palabra
-
 /*
   cout<< "Imprimir por pantalla"<< endl;
   cout<<  "Imprimir por pantalla \n "
@@ -42,6 +59,12 @@ cout<< repetir;
   }
 }*/
 /*
+do {
+  cout<< "Intoduce la contrasenia: ";
+  cin>> repetir;
+}while(repetir != contrasena);
+*/
+/*
 cout<< "Cuantos anos tienes?";
 cin>> edad2;
 if (edad2>=edad ){
@@ -57,6 +80,30 @@ if (diferencia){
 }
 numero= valorAbsoluto(-1);
 cout<< numero<<endl;*/
+
+for (int i=0;i<=1000;i+=2){
+  suma+=i ;
+
+
+
+}
+cout <<"La suma de los 1000 primeros numeros es :"<< suma<<endl;
+
+int arr[]= {10,20,30,40};
+for (int i:arr){
+  cout<< i<<endl;
+}
+numMax=30;
+numMin=0;
+cout<< "Introduce un numero " ;
+cin>> num1 ;
+ok = meSirve(numMax,numMin,num1);
+cout<< "Introduce un numero 2 " ;
+cin>> num2 ;
+ok2 = meSirveV2(numMax,numMin,num2);
+imprimeBool(ok);
+cout<<endl;
+imprimeBool(ok2);
 
   return 0;
 }
