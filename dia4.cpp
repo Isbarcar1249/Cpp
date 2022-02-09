3567c8d6389c497cf6cd16fd5cfc7125b1721c8b
ismael <ismael.barrranco@itgranada.com>
2022-02-09 12:07:55 +0100

    Vemos punteros

--- dia4.cpp
+++ dia4.cpp
@@ -4,16 +4,18 @@ using namespace std;
 int main(){
   string juego= "Tragabolas";
   string &ocio=juego;
-  string juegos[5];
+  int num=5;
+  //string juegos[5];
+string* ptrJuego =&juego;
+int* ptrNum= &num;
+  //cout<<juego<<endl;//imprime el juego
+//  cout<<ocio<<endl;//imprime el juego
 
-  cout<<juego<<endl;//imprime el juego
-  cout<<ocio<<endl;//imprime el juego
 
-
-  cout<<&juego<<endl;// imprime la dirección
+  /*cout<<&juego<<endl;// imprime la dirección
   cout<<&ocio<<endl;// imprime la dirección
 
-  ocio= "pokemon";
+  //ocio= "pokemon";
 
   cout<<ocio<<endl;
   cout<<juego<<endl;
@@ -23,9 +25,16 @@ int main(){
   cout<<&juegos[1]<<endl;
   cout<<&juegos[2]<<endl;
   cout<<&juegos[3]<<endl;
-  cout<<&juegos[4]<<endl;
+  cout<<&juegos[4]<<endl;*/
+cout<<"Imprimo el juego "<<juego <<endl;
+cout<<"Imprimo el &juego "<< &juego<<endl;
+cout<<"Imprimo el ocio  "<<ocio<<endl;
+cout<<"Imprimo el &ocio  "<<&ocio<<endl;
+cout<<"Imprimo el ptrJuego: "<<ptrJuego<<endl;
 
-
+cout<<"Imprimo el *ptrJuego: "<<*ptrJuego<<endl;
+cout<<"Imprimo el &num: "<<&num<<endl;
+cout<<"Imprimo el ptrNum: "<<ptrNum<<endl;
 
 
 
