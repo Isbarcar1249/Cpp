02c69256dcd6f269ff2208433b017affd127c62f
ismael <ismael.barrranco@itgranada.com>
2022-02-09 21:20:50 +0100

    Ejercicios hechos, los comentarios eran para comprobar cosas :D

--- matrices.cpp
+++ matrices.cpp
@@ -1,24 +1,81 @@
 #include <iostream>
 using namespace std;
 int const TAMA=10;
-void rellenarMatrizNumero(int matriz[TAMA][TAMA],int nFilas,int nCol,int num){
+void rellenarMatrizNumero(int matriz[TAMA][TAMA],int nFilas,int nCol){
+  int num;
   for (int i=0; i<nFilas; i++){
     for(int j=0; j<nCol; j++){
+      cout<<"Dime el numero de la posicion ["<<i<<"]["<<j<<"] ";
+      cin>>num;//Para rellenarla con 0, introducir 0 en este momento
       matriz[i][j]=num;
     }
   }
 }
+void mostrarMatriz(int matriz[TAMA][TAMA],int nFilas,int nCol ){
+  for(int i=0;i<nFilas;i++){
+    for(int j=0;j<nCol;j++){
+      cout<<matriz[i][j] <<" ";
+    }
+    cout<<endl;
+  }
+}
+void mostrarMatrizSTR(string matriz[TAMA][TAMA],int nFilas,int nCol ){
+  for(int i=0;i<nFilas;i++){
+    for(int j=0;j<nCol;j++){
+      cout<<matriz[i][j] <<" ";
+    }
+    cout<<endl;
+  }
+}
+void hacerTablero(string tablero[TAMA][TAMA],int nFilas,int nCol){
+  int contador=0;
 
+  cout<<"Cuantas filas quieres que tenga tu tablero ";
+  cin>>nFilas;
+  cout<<"Cuantas columnas quieres que tenga tu tablero ";
+  cin>>nCol;
+  for (int i=0; i<nFilas; i++){
+    //cout<<"hola" <<i%2;
+    if (i%2==1){
+      contador=1;
+    }
+    else{
+      contador=0;
+    }
+    for(int j=0; j<nCol; j++){
+      if (contador%2==0 ){
+        tablero[i][j]="X ";
+        //cout<<"hola";
+      }else{
+        tablero[i][j]="O ";
+      //  cout<<"Adios";
+
+      }
+      contador++;
+      cout<<tablero[i][j];
+
+    }cout<<endl;
+
+
+  }
+
+}
 
 int main(){
 
 int array[TAMA];
 int matriz[TAMA][TAMA];
-rellenarMatrizNumero(matriz,2,2,4);
-cout<<matriz[1][0]<<endl;
+string tablero[TAMA][TAMA];
+int filas,columnas;
+cout<<"Cuantas filas quieres que tenga tu matriz ";
+cin>>filas;
+cout<<"Cuantas columnas quieres que tenga tu matriz ";
+cin>>columnas;
 
-
-
+rellenarMatrizNumero(matriz,filas,columnas);
+mostrarMatriz(matriz,filas,columnas);
+hacerTablero(tablero,2,2);
+//mostrarMatrizSTR(tablero,10,10);
 
   return 0;
 }
